9579f34e6bd6ea8c76901d931eb224c174974dca
Guilherme22-Dev <gui_dionisio@hotmail.com>
2026-03-15 01:29:21 +0000

    fix vercel bug white screen

--- src/app.tsx
+++ src/app.tsx
@@ -1,18 +1,30 @@
-import { Header } from "./components/Header";
-import { Hero } from "./components/Hero";
-import { Services } from "./components/Services";
-import { About } from "./components/About";
-import { Contact } from "./components/Contact";
-import { Footer } from "./components/Footer";
+// src/App.tsx
+import { Header } from "./components/header";
+import { Hero } from "./components/hero";
+import { Services } from "./components/services";
+import { About } from "./components/about";
+import { Contact } from "./components/contact";
+import { Footer } from "./components/footer";
 
 export default function App() {
   return (
-    <div className="w-full scroll-smooth">
+    <div className="w-full scroll-smooth font-sans bg-black text-white">
+      {/* Header fixo */}
       <Header />
+
+      {/* Hero Section */}
       <Hero />
+
+      {/* Serviços */}
       <Services />
+
+      {/* Sobre */}
       <About />
+
+      {/* Contato */}
       <Contact />
+
+      {/* Footer */}
       <Footer />
     </div>
   );
